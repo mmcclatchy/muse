84272b1b846dd592cfcec0fe4841d2b2a7b1789c
mark <markmcclatchy@gmail.com>
2020-12-06 13:25:45 -0500

    added trait route

---
+++ routes/api/traits.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { requireAuth } = require('../../auth');
+const { Trait, TraitType } = require('../../db/models');
+const { normalize, sortTraits } = require('../../utilities')
+
+const router = express.Router();
+
+
+// Get all Character Traits and Trait Types
+router.get('/',
+  // requireAuth, 
+  asyncHandler(async (req, res) => {
+  const traits = await Trait.findAll({
+    attributes: ['id', 'name', 'typeId'],
+    include: [{
+      model: TraitType,
+      attributes: ['type']
+    }]
+  })
+  
+  const sortedTraits = sortTraits(traits)
+  
+  res.json({ payload: sortedTraits })
+}))
+
+
+// Post a new character Trait
+router.post('/', requireAuth, asyncHandler(async (req, res) => {
+  
+}));
+
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
-const router = require('express').Router();
-const api = require('./api');
+const fs = require('fs')
+const router = require("express").Router();
 
-router.use('/api', api);
+// Read all file names within the ./api directory
+const fileNames = fs.readdirSync('./routes/api');
+
+// slice the file extensions off
+const routes = fileNames.map(fileName => fileName.slice(0,fileName.indexOf('.')))
+
+// loop though routes to establish them in the application
+for (let route of routes) {
+  router.use(`/api/${route}`, require(`./api/${route}`));
+}
 
 module.exports = router;
