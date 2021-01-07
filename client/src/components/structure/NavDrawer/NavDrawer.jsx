9767467467989737ea686bfc86cf82a64b695866
mark <markmcclatchy@gmail.com>
2021-01-07 15:18:23 -0500

    drawer opening and closing correctly

--- client/src/components/structure/NavDrawer/NavDrawer.jsx
+++ client/src/components/structure/NavDrawer/NavDrawer.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './nav_drawer.css';
+import { useSelector } from 'react-redux';
 
 
 
 export default function NavDrawer() {
+  const drawerOpen = useSelector(state => state.navigation.drawerOpen);
   
+  useEffect(() => {console.log('Drawer Toggle')}, [drawerOpen]);
   
   return (
-    <div className="nav-drawer">
+    <div className={drawerOpen ? 'nav-drawer open' : 'nav-drawer'}>
       
     </div>
   )
