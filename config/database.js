5be6bd12ba27cd174b2e20013ebf50464b7e4041
Mark <markmcclatchy@gmail.com>
2021-07-05 15:13:33 -0400

    add ssl for heroku

--- config/database.js
+++ config/database.js
@@ -24,5 +24,9 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
+    ssl: true, 
+    dialectOptions: {
+      ssl: true
+    }
   },
 };
